4ce6dab0854049175b8f7b5f712532366ed9792f
hritikcps <hritik.raj@cloudprism.in>
2024-04-12 11:42:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import BookingForm from './components/form'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Schedule from './components/schedule'
+
+
+function App() {
+
+  return (
+
+    // <div>
+    //   <BookingForm />
+
+    // </div>
+
+    <Router>
+      <Routes>
+
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/" element={<BookingForm />} />
+
+
+
+
+
+
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App
